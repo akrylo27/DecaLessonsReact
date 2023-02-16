82f8fab7a5c5e5c04aeed082534fb6b00049c9db
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-02-16 15:56:52 +0300

    Add SASS and add folder SASS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles', 'scss')],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
