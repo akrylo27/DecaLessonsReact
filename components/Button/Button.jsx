43da5caf4daf0dcff982bcff7e4b5418616ba0ab
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-02-21 14:33:51 +0300

    a to Link

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,14 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Button = ({ text, type, style, link, ...props }) => {
   const renderSwitchButton = (style, link) => {
     if (link) {
       return (
-        <a href={link}>
+        <Link href={link}>
           <button className={'button button__' + style} type={type} {...props}>
             <p>{text}</p>
           </button>
-        </a>
+        </Link>
       );
     } else {
       return (
