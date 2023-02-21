08c196c62bb7321e6cdb9505073bf2d7346a7bbd
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-02-21 14:32:03 +0300

    Fix swtich to composition

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -2,55 +2,20 @@ import React from 'react';
 
 const Button = ({ text, type, style, link, ...props }) => {
   const renderSwitchButton = (style, link) => {
-    switch (style) {
-      case 'grey':
-        if (link) {
-          return (
-            <a href={link}>
-              <button className='button button__grey' type={type} {...props}>
-                <p>{text}</p>
-              </button>
-            </a>
-          );
-        } else {
-          return (
-            <button className='button button__grey' type={type} {...props}>
-              <p>{text}</p>
-            </button>
-          );
-        }
-      case 'orange':
-        if (link) {
-          return (
-            <a href={link}>
-              <button className='button button__orange' type={type} {...props}>
-                <p>{text}</p>
-              </button>
-            </a>
-          );
-        } else {
-          return (
-            <button className='button button__orange' type={type} {...props}>
-              <p>{text}</p>
-            </button>
-          );
-        }
-      default:
-        if (link) {
-          return (
-            <a href={link}>
-              <button className='button' type={type} {...props}>
-                <p>{text}</p>
-              </button>
-            </a>
-          );
-        } else {
-          return (
-            <button className='button' type={type} {...props}>
-              <p>{text}</p>
-            </button>
-          );
-        }
+    if (link) {
+      return (
+        <a href={link}>
+          <button className={'button button__' + style} type={type} {...props}>
+            <p>{text}</p>
+          </button>
+        </a>
+      );
+    } else {
+      return (
+        <button className={'button button__' + style} type={type} {...props}>
+          <p>{text}</p>
+        </button>
+      );
     }
   };
 
