b5dc6ce47cc8fd87ffec0a1a519cddc34b13d910
Aleksey <aleksey.kosourov@decathlon.com>
2023-02-23 00:15:44 +0300

    update

--- components/form/Registration.jsx
+++ components/form/Registration.jsx
@@ -1,7 +1,5 @@
 import React from "react"
 import Input from "./Input"
-/* import Password from "./Password"
-import Email from "./Email" */
 import Checkbox from "./Checkbox"
 import Button from "./Button"
 import Label from "./Label"
