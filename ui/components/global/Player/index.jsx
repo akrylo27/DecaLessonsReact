4a68032ebd576c5a8830f3f85c7b48e38ddb7836
akrylo27 <61235006+akrylo27@users.noreply.github.com>
2023-03-01 18:37:43 +0300

    fixed player

--- ui/components/global/Player/index.jsx
+++ ui/components/global/Player/index.jsx
@@ -6,6 +6,7 @@ import styles from '@/ui/components/global/Player/Player.module.scss'
 export default function Player({ track }) {
   const [audio, setAudio] = useState(null);
   const [isPlay, setIsPlay] = useState(true);
+  const [isPlayMove, setIsPlayMove] = useState(false);
   const [percentage, setPercentage] = useState(0);
   const [fullTime, setfullTime] = useState('00:00');
   const [currentTime, setCurrentTime] = useState('00:00');
@@ -41,13 +42,21 @@ export default function Player({ track }) {
     audio.pause();
   };
 
-  const rewind = (e) => {
-    const rect = e.target.getBoundingClientRect();
-    const pageX = e.pageX;
-    const rewind = (pageX / rect.width) * audio.duration;
+  const rewind = (e, active) => {
+    setIsPlayMove(active)
 
-    audio.currentTime = rewind;
-    audioTimeUpdate();
+    if (active) {
+      const rect = e.target.getBoundingClientRect();
+      const pageX = e.pageX;
+      const rewind = (pageX / rect.width) * audio.duration;
+
+      audio.currentTime = rewind;
+      audio.muted = true
+      audioTimeUpdate();
+      return
+    }
+
+    audio.muted = false
   };
 
   const rewindMove = (e) => {
@@ -72,7 +81,13 @@ export default function Player({ track }) {
   return (
     <>
       <div className={styles.player} id="player" onMouseMove={rewindMove}>
-        <div className={styles.playerBar} onClick={rewind}>
+        <div
+          className={styles.playerBar}
+          onMouseMove={(e) => rewind(e, isPlayMove)}
+          onMouseDown={(e) => rewind(e, true)}
+          onMouseUp={(e) => rewind(e, false)}
+          onMouseLeave={(e) => rewind(e, false)}
+        >
           <div className={styles.playerBar__progressMove} time={currentTimeMove}></div>
           <div className={styles.playerBar__progress} style={{width: `${percentage}%`}}></div>
         </div>
