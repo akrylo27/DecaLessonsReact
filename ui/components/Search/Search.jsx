7ae88bc80f002f7be67164a2595b87ee02c61038
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-03-06 23:02:38 +0300

    Search done, but without style

--- ui/components/Search/Search.jsx
+++ ui/components/Search/Search.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 import { HiMagnifyingGlass } from 'react-icons/hi2';
 import { IconContext } from 'react-icons';
@@ -7,23 +6,41 @@ import Input from '../form/Input';
 
 const Search = () => {
   const [artists, setArtist] = useState([]);
-  const [query, setQuery] = useState('');
-
+  const [inputValue, setinputValue] = useState('');
+  const token =
+    '8772fc18d6e25c48aebf0274c2e977db022199dc0bde9e3c5e1fc7e9e216df31475ee2277479e194c191db2d0fb3aba2ee2e48d35ea37cdfa1b1d5fb72e415e9ed644b1c1fc676e183858323db98d8729ee0ce2d8dfacf55e7647e130ff9dc7d41e9475a30723f419a0a39f8e51b57ae20dd3086df669204fbe03a2f6209d2e5';
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(`https://api.dless.ru/api/audios`);
-        setArtist(data.data);
-      } catch (error) {
-        console.log(error);
+    try {
+      if (inputValue.length > 0) {
+        fetch('https://api.dless.ru/api/audios', {
+          method: 'get',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then(async (response) => {
+          setArtist([]);
+          const { data } = await response.json();
+          let searchQuery = inputValue.toLowerCase();
+          for (const key in data) {
+            let author = data[key].attributes.author.toLowerCase();
+            if (author.match(searchQuery)) {
+              setArtist((prevResult) => {
+                return [...prevResult, data[key].attributes.author];
+              });
+            }
+          }
+        });
+      } else {
+        setArtist([]);
       }
-    };
-    fetchData();
-  }, [query]);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [inputValue]);
 
   const handleChangeFilter = (e) => {
     e.preventDefault();
-    setQuery(e.target.value);
+    setinputValue(e.target.value);
   };
 
   return (
@@ -35,21 +52,14 @@ const Search = () => {
           placeholder={'Трек, Артист...'}
           size={'xg'}
           variant={'dark'}
-          value={query}
+          value={inputValue}
           onChange={handleChangeFilter}
         />
-      </div>
-
-      <div>
-        {artists.map((artist) => {
-          return (
-            <div key={artist.id}>
-              <ul>
-                <li>{artist.id}</li>
-              </ul>
-            </div>
-          );
-        })}
+        <div>
+          {artists.map((artists, index) => (
+            <div key={index}>{artists}</div>
+          ))}
+        </div>
       </div>
     </IconContext.Provider>
   );
