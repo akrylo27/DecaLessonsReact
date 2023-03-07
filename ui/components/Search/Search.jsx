b703bc2922008b4325caf664e3bafd9d6216d1ab
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-03-07 15:08:50 +0300

    Add serch with name and author + fix style

--- ui/components/Search/Search.jsx
+++ ui/components/Search/Search.jsx
@@ -5,7 +5,7 @@ import { IconContext } from 'react-icons';
 import Input from '../form/Input';
 
 const Search = () => {
-  const [artists, setArtist] = useState([]);
+  const [serchAudio, setserchAudio] = useState([]);
   const [inputValue, setinputValue] = useState('');
   const token =
     '8772fc18d6e25c48aebf0274c2e977db022199dc0bde9e3c5e1fc7e9e216df31475ee2277479e194c191db2d0fb3aba2ee2e48d35ea37cdfa1b1d5fb72e415e9ed644b1c1fc676e183858323db98d8729ee0ce2d8dfacf55e7647e130ff9dc7d41e9475a30723f419a0a39f8e51b57ae20dd3086df669204fbe03a2f6209d2e5';
@@ -18,20 +18,27 @@ const Search = () => {
             Authorization: `Bearer ${token}`,
           },
         }).then(async (response) => {
-          setArtist([]);
+          setserchAudio([]);
           const { data } = await response.json();
           let searchQuery = inputValue.toLowerCase();
           for (const key in data) {
-            let author = data[key].attributes.author.toLowerCase();
-            if (author.match(searchQuery)) {
-              setArtist((prevResult) => {
-                return [...prevResult, data[key].attributes.author];
-              });
+            let music = [
+              data[key].attributes.author.toLowerCase() +
+                ' - ' +
+                data[key].attributes.name.toLowerCase(),
+            ];
+
+            for (const key in music) {
+              if (music[key].match(searchQuery)) {
+                setserchAudio((prevResult) => {
+                  return [...prevResult, music[key]];
+                });
+              }
             }
           }
         });
       } else {
-        setArtist([]);
+        setserchAudio([]);
       }
     } catch (error) {
       console.log(error);
@@ -51,29 +58,29 @@ const Search = () => {
   return (
     <IconContext.Provider value={{ className: 'magnifyingglass' }}>
       <div className='search'>
-        <div className='search_input'>
-          <HiMagnifyingGlass />
-          <Input
-            type={'text'}
-            placeholder={'Трек, Артист...'}
-            size={'xg'}
-            variant={'dark'}
-            value={inputValue}
-            onChange={handleChangeFilter}
-          />
-        </div>
+        <HiMagnifyingGlass />
+        <Input
+          type={'text'}
+          placeholder={'Трек, Артист...'}
+          size={'xg'}
+          variant={'dark'}
+          value={inputValue}
+          onChange={handleChangeFilter}
+        />
         <div className='live'>
-          {artists.map((artists, index) => (
-            <Input
-              key={index}
-              onClick={() => handleClickFilter(artists)}
-              value={artists}
-              type={'text'}
-              size={'xg'}
-              variant={'dark'}
-              cursor={'pointer'}
-              readOnly
-            />
+          {serchAudio.map((name, index) => (
+            <div className='item'>
+              <Input
+                key={index}
+                onClick={() => handleClickFilter(name)}
+                value={name}
+                type={'text'}
+                size={'xg'}
+                variant={'dark'}
+                cursor={'pointer'}
+                readOnly
+              />
+            </div>
           ))}
         </div>
       </div>
