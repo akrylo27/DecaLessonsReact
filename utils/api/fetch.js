1e5a6e7e8efe8f1b6aee47d74994c4f78b029c6a
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-03-08 17:21:16 +0300

    Add fetch

--- utils/api/fetch.js
+++ utils/api/fetch.js
@@ -1,22 +1,26 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
 
-const fetchAPI = async (url, request) => {
+const fetchAPI = async (url, request, parms = null) => {
   try {
     switch (request) {
       case 'get':
         const response = await fetch(`${API_URL}/${url}`, {
-          method: request,
+          method: 'get',
           headers: {
             Authorization: `Bearer ${API_TOKEN}`,
           },
         });
         return response;
       case 'post':
-        console.log('post');
-        break;
-      default:
-        console.log('get');
+        await fetch(`${API_URL}/${url}`, {
+          method: 'post',
+          headers: {
+            Authorization: `Bearer ${API_TOKEN}`,
+          },
+          body: JSON.stringify(parms),
+        });
+        console.log(parms);
         break;
     }
   } catch (error) {
