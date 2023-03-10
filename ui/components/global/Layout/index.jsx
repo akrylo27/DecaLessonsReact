ae276ad1505e1e28a5be83451a0acf992e48cf8e
akrylo27 <61235006+akrylo27@users.noreply.github.com>
2023-03-10 13:15:00 +0300

    fix build

--- ui/components/global/Layout/index.jsx
+++ ui/components/global/Layout/index.jsx
@@ -1,6 +1,5 @@
 import Header from '@/ui/components/global/Header';
 import Player from '@/ui/components/global/Player';
-import Preloader from '@/ui/components/global/Preloader'
 
 export default function Layout({ children }) {
   const track = {
@@ -14,10 +13,6 @@ export default function Layout({ children }) {
     <div>
       <Header />
       {children}
-
-      <div className='container'>
-        <Preloader />
-      </div>
 
       <Player track={track} />
     </div>
