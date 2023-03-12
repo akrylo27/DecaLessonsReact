b6f5d06c6780fa8cf6c572c4cbbc1bb00d989a28
Aleksandr <salikweb@yandex.ru>
2023-03-12 18:25:22 +0300

    add next auth

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { signOut } from "next-auth/react"
 
 const profile = () => {
-  return <div>Page Profile</div>;
+  const handle = () => {
+    signOut()
+  }
+
+  return (
+    <div className='container'>
+      <button onClick={handle}>Log Out</button>
+    </div>
+  );
 };
 
 export default profile;
--- ui/components/global/Header/index.tsx
+++ ui/components/global/Header/index.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
 
 import Logo from '@/ui/components/global/Logo';
 import Navigation from '@/ui/components/global/Navigation';
@@ -8,23 +7,6 @@ import AuthBadge from '@/ui/components/global/AuthBadge';
 import styles from './Header.module.scss';
 
 const Header = () => {
-  // Переделаем на cookie
-  const [savedJWT, setSevedJWT] = useState('');
-  const router = useRouter();
-
-  useEffect(() => {
-    if (JSON.parse(localStorage.getItem('jwt'))) {
-      setSevedJWT(JSON.parse(localStorage.getItem('jwt')));
-    } else {
-      setSevedJWT('');
-    }
-  });
-
-  const handleClickLogOut = () => {
-    localStorage.removeItem('jwt');
-    router.push('/');
-  };
-
   return (
     <>
       <div className={styles.headerInfo}>
