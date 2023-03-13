7767f7f3981f23fae775f1d455155c1e05ba7d1b
Aleksandr <salikweb@yandex.ru>
2023-03-13 11:21:49 +0300

    add ava user

--- ui/components/global/AuthBadge/index.jsx
+++ ui/components/global/AuthBadge/index.jsx
@@ -1,10 +1,15 @@
-import Link from 'next/link';
+import Link from 'next/link'
+import Image from 'next/image'
 import { useSession, signIn, signOut } from "next-auth/react"
 
 import styles from './AuthBadge.module.scss'
 
 const AuthBadge = () => {
   const { data: session, status, data } = useSession()
+
+  const customLoader = () => {
+    return `https://api.dless.ru${session?.user?.image}`
+  }
 
   console.log('session', session)
   console.log('status', status)
@@ -15,7 +20,16 @@ const AuthBadge = () => {
       <div className={styles.authBadge__name}>
         {session ? session?.user?.name ? session?.user?.name : session?.user?.username : 'Авторизация' }
       </div>
-      <div className={styles.authBadge__ava}></div>
+      <div className={styles.authBadge__ava}>
+        {session?.user?.image && <Image
+          src="profilePic.webp"
+          width={34}
+          height={34}
+          alt="User profile picture"
+          quality={80}
+          loader={customLoader}
+        />}
+      </div>
     </Link>
   )
 }
